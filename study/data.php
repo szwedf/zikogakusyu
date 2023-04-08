e67c3081938384bc27ebae1a7ff65191d58baf9f
szwedf <95062731+szwedf@users.noreply.github.com>
2023-04-08 13:56:26 +0900

    Add files via upload

---
+++ study/data.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        //四則演算（整数型)
+        echo 45 + 18;
+        echo '<br>';
+        echo 30 - 12;
+        echo '<br>';
+        echo 15 * 6;
+        echo '<br>';
+        echo 30 / 3;
+        echo '<br>';
+        echo 45 % 8;
+        ?>
+    </p>
+</body>
+
+</html>
